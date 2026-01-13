05a74e984a23bf5dfb5d6395e88587dff9a47d06
manishhsuthar <manishsuthar.dev@gmail.com>
2026-01-13 23:10:37 +0530

    route matching

--- SPA_Backend/apps/academics/models.py
+++ SPA_Backend/apps/academics/models.py
@@ -8,10 +8,11 @@ User = settings.AUTH_USER_MODEL
 
 class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
-    full_name = models.CharField(max_length=100, blank=False)
-    enrollment_number = models.CharField(max_length=50)
-    institute_name = models.CharField(max_length=200)
-    current_semester = models.PositiveSmallIntegerField()
+    full_name = models.CharField(max_length=100, blank=True)
+    enrollment_number = models.CharField(max_length=50, unique=True)
+    institute_name = models.CharField(max_length=200, blank=True)
+    current_semester = models.PositiveSmallIntegerField(blank=True, null=True)
+    interests = models.TextField(blank=True)
 
     def __str__(self):
         return self.user.email
